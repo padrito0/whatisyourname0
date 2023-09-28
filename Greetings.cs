fa93ddb242967cb3fb39ecbdbc415b0c6b74ffe9
padrito0 <146373717+padrito0@users.noreply.github.com>
2023-09-28 12:36:43 -0500

    Add files via upload

---
+++ Greetings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Greetings 
+{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            // Asking for user's name
+            Console.WriteLine("Hello! What is your name?");
+            
+            // User inputs name
+            string? name = Console.ReadLine(); 
+
+            // Greet the user
+            Console.WriteLine("It is nice to meet you, " + name + "!");
+
+            // Stop program
+            Console.ReadKey(); 
+
+        }
+    }
+}
